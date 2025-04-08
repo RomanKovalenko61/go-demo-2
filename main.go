2747640ff277cc29ce56cd65f7a70659141400e0
Roman <astek14@mail.ru>
2025-04-08 15:10:01 +0300

    6.3 Slice

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	transactions := [3]int{1, 2, 3}
+	transactions := [5]int{1, 2, 3, 4, 5}
 	banks := [2]string{"Tinkoff", "Alfa"}
 
 	fmt.Println(transactions[1])
 	banks[0] = "Sber"
 	fmt.Println(banks)
+
+	partial := transactions[1:4] // partial := transactions[1:] // partial := transactions[:4]
+	fmt.Println(partial)
 }
