aecb07ee9fafd58291e386eb997b037064e244bd
Roman <astek14@mail.ru>
2025-04-08 15:50:36 +0300

    6.5 Dynamic array (slice)

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	transactions := [6]int{1, 2, 3, 4, 5, 6}
-	transactionsPartial := transactions[1:5]
-	transactionsNewPartial := transactionsPartial[:1]
-	transactionsNewPartial[0] = 30
-
-	transactionsNewPartial = transactionsNewPartial[0:5]
-
-	fmt.Println(transactions)
-	fmt.Println(transactionsNewPartial)
-	fmt.Println(transactionsPartial, len(transactionsPartial), cap(transactionsPartial))
-	fmt.Println(transactionsNewPartial, len(transactionsNewPartial), cap(transactionsNewPartial))
+	transactions := []int{1, 2, 3, 4, 5, 6}
+	temp := transactions
+	transactions = append(transactions, 100)
+	fmt.Println(transactions[1:])
+	fmt.Println(temp)
 }
